017b2c88fcb079aedc4f7d1b33f0775840d73cb9
Sander van Kasteel <info@sandervankasteel.nl>
2017-08-08 22:35:05 +0200

    Added some basic routing

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//
+//});
+
+Route::get('/', 'RoomsController@index');
+Route::get('/room{room}', 'RoomsController@room');
