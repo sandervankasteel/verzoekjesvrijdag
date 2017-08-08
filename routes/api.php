777cea4b170a1d47e1ff66f03dfc9a384513c8fb
Sander van Kasteel <info@sandervankasteel.nl>
2017-08-08 22:33:51 +0200

    Started working on a basic API

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+// 'middleware' => [''],
+// /api/v1 should use a middleware
+Route::group([ 'prefix' => 'v1'], function() {
+    Route::post('search/youtube', 'YoutubeController@search');
 });
