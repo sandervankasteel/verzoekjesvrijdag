d9a8de7f9343359663447957333bd8f28b09acc3
Sander van Kasteel <info@sandervankasteel.nl>
2017-08-10 23:11:01 +0200

    Tried somethings with Vue

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -1,23 +1,13 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <a class="mdl-list__item-secondary-action" href="#"  v-on:click="handleClick" ><i class="material-icons">thumb_up</i></a>
 </template>
-
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+    modules.export = {
+        el: '.vote-up',
+        methods: {
+            handleClick: function(event) {
+                console.log("handleClick has been pressed");
+            }
         }
     }
 </script>
