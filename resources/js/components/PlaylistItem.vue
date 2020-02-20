fec6d2ec5e43427d6baf6f29e799115e2769a43f
Sander van Kasteel <info@sandervankasteel.nl>
2020-02-20 13:24:37 +0100

    Initial implementation of VoteButton

--- resources/js/components/PlaylistItem.vue
+++ resources/js/components/PlaylistItem.vue
@@ -10,30 +10,48 @@
         </div>
         <div class="flex mb-4">
             <div class="w-1/2">
-<!--                <font-awesome-icon :icon="['far', 'fa-thumbs-up']"/>-->
-                Upvotes: {{ item.upvotes }}
+                <div class="bg-white h-8 w-16 rounded-lg shadow inline-block flex items-center font-bold" v-on:click="handleVote('up', $event)">
+                    <font-awesome-icon icon="thumbs-up" class="mr-2 ml-2"/>
+                    <span class="text-green-500">{{ item.upvotes }}</span>
+                </div>
             </div>
             <div class="w-1/2">
-<!--                <font-awesome-icon :icon="['far', 'fa-thumbs-down']"/>-->
-                Downvotes: {{ item.downvotes }}
+<!--                <div class="bg-white h-8 w-16 rounded-lg shadow inline-block flex items-center font-bold" v-on:click="handleVote('down', $event)">-->
+<!--                    <font-awesome-icon icon="thumbs-down" class="mr-2 ml-2"/>-->
+<!--                    <span class="text-red-500">{{ item.downvotes }}</span>-->
+<!--                </div>-->
+                <VoteButton v-bind:votes="item.downvotes"></VoteButton>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import VoteButton from "./VoteButton";
     export default {
         name: "PlaylistItem",
+        components: {VoteButton},
         props: ["item"],
         methods: {
-            handleVote: function(type) {
-                // set animation type to bounce
-                console.log('vote has been cast')
+            handleVote: function(type, event) {
+
+                let element = event.target;
+                let spanEl = element.querySelector('span');
+                let value = parseInt(spanEl.textContent);
+                if (type === 'up') {
+                    spanEl.textContent = value + 1;
+                } else {
+                    spanEl.textContent = value - 1;
+                }
+
+                element.classList.add('animated', 'tada')
             }
         }
     }
 </script>
 
 <style scoped>
-
+    font-awesome-icon {
+        cursor: pointer;
+    }
 </style>
