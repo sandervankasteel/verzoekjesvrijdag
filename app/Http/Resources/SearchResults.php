6883d032377923a857927221bd2ed58c08e75dbe
Sander van Kasteel <info@sandervankasteel.nl>
2020-02-23 21:12:22 +0100

    Added Search Request FormRequest and SearchResults response
    Signed-off-by: Sander van Kasteel <info@sandervankasteel.nl>

---
+++ app/Http/Resources/SearchResults.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SearchResults extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id->videoId,
+            'title' => $this->snippet->title,
+            'image_url' => $this->snippet->thumbnails->default->url
+        ];
+    }
+}
