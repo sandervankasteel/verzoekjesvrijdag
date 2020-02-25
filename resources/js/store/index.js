6e74ffbdb0e145946b6592b8d73481ef2c5cd986
Sander van Kasteel <info@sandervankasteel.nl>
2020-02-25 22:20:37 +0100

    Import room for state usage
    Signed-off-by: Sander van Kasteel <info@sandervankasteel.nl>

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import playlist from "./modules/playlist";
+import room from "./modules/room";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        playlist
+        playlist,
+        room
     }
 });
