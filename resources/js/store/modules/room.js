16d8d422102d876cc15ea1328414eaf78002a50e
Sander van Kasteel <info@sandervankasteel.nl>
2020-02-29 20:33:38 +0100

    Added addMembers functionality

--- resources/js/store/modules/room.js
+++ resources/js/store/modules/room.js
@@ -1,3 +1,5 @@
+import Echo from "laravel-echo";
+
 const state = {
     id: '',
     members: [],
@@ -8,9 +10,19 @@ const getters = {};
 const actions = {
     setRoom({ state, commit }, roomId) {
         commit('setRoomId', roomId);
+
+        window.Echo.channel(roomId).listen('users', function(data) {
+            console.log('Echo reply: ', data);
+        });
+
     },
     addMember({ state, commit}, name) {
         commit('appendMember', name)
+    },
+    addMembers({ state, commit}, users) {
+        for(let user of users) {
+            commit('appendMember', user);
+        }
     }
 };
 
