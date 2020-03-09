d1d0602b0a32d7f5a47ce5b6d3d2512e6a726343
Sander van Kasteel <info@sandervankasteel.nl>
2020-03-09 22:55:05 +0100

    Added an initial version of the ItemAddedToPlaylist event

---
+++ app/Events/ItemAddedToPlaylist.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\PlaylistItem;
+use App\Models\Room;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ItemAddedToPlaylist implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Room $room
+     */
+
+    private $room;
+
+    public $item;
+
+    public function __construct(Room $room, PlaylistItem $item)
+    {
+        $this->room = $room;
+
+        $this->item = $item;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('room' . $this->room->name . 'items');
+    }
+}
