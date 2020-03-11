ea942503e9c087f8047fd0e70fbc59a36f1d936f
Sander van Kasteel <info@sandervankasteel.nl>
2020-03-11 22:14:46 +0100

    Added title and image_url to playlistitems

--- database/migrations/2020_02_22_223918_create_playlistitem_table.php
+++ database/migrations/2020_02_22_223918_create_playlistitem_table.php
@@ -15,8 +15,10 @@ class CreatePlaylistitemTable extends Migration
     {
         Schema::create('playlistitems', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('room_id');
+            $table->string('title', 255);
+            $table->string('image_url', 255);
             $table->string('youtube_id', 255);
+            $table->unsignedBigInteger('room_id');
 
             $table->timestamps();
             $table->foreign('room_id')->references('id')->on('rooms');
