b338ffaae8de8b90e72ff71a9745ca2a0cacd760
Sander van Kasteel <info@sandervankasteel.nl>
2020-03-14 22:25:45 +0100

    Require Axios first and set the X-Request-With header.
    Changing this, fixes the issue that Laravel Echo couldn't set the socketId.
    
    Signed-off-by: Sander van Kasteel <info@sandervankasteel.nl>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 // /**
 //  * First we will load all of this project's JavaScript dependencies which
 //  * includes Vue and other libraries. It is a great starting point when
