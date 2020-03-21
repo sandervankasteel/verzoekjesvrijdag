d2fc25abefcbaae73728fa3b136148b6f7aa61d2
Sander van Kasteel <info@sandervankasteel.nl>
2020-03-21 23:05:35 +0100

    Changed allot of stuff

--- resources/js/store/modules/playlist.js
+++ resources/js/store/modules/playlist.js
@@ -1,5 +1,3 @@
-const axios = window.axios;
-
 const state = {
     items: [{
         youtube_id: 'dQw4w9WgXcQ',
@@ -7,24 +5,40 @@ const state = {
         title: 'Rick Astley - Never Gonna Give You Up',
         upvotes: 10,
         downvotes: 1
+    }, {
+        youtube_id: 'QwdbFNGCkLw',
+        'image_url': 'https://i.ytimg.com/vi/QwdbFNGCkLw/default.jpg',
+        title: 'Nightcore - Ievan Polkka (VSNS Remix)',
+        upvotes: 420,
+        downvotes: 69
     }],
 };
 
 // getters
-const getters = {};
+const getters = {
+    getItems: state => {
+        return state.items;
+    },
+    getNextInQueue: state => {
+        return state.items[0];
+    }
+};
 
 // actions
 const actions = {
     addToPlaylist({ state, commit, rootState }, item) {
         const roomName = rootState.room.id;
         commit('pushToPlaylist', { item, roomName });
+    },
+    removeFirstFromPlaylist({ state, commit}) {
+        commit('popFromPlaylist')
     }
 };
 
 const mutations = {
     pushToPlaylist( state, { item, roomName }) {
         if(item.pushToBackend) {
-            axios.post(`/api/v1/rooms/${roomName}/add_to_playlist`, item);
+            window.axios.post(`/api/v1/rooms/${roomName}/add_to_playlist`, item);
         }
 
         // if(state.items[state.items.length -1].youtube_id !== item.youtube_id) {
@@ -34,6 +48,9 @@ const mutations = {
                 ...item
             });
         // }
+    },
+    removeFromPlaylist( state ) {
+        state.items = state.items.slice(1);
     }
 };
 
