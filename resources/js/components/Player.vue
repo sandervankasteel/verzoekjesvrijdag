9ac0f7fbd4309b81d7576407fd898e0eaeb2f4f7
Sander van Kasteel <info@sandervankasteel.nl>
2020-03-21 22:54:10 +0100

    Added an initial version of the Player component

--- resources/js/components/Player.vue
+++ resources/js/components/Player.vue
@@ -1,10 +1,48 @@
 <template>
-
+    <youtube v-if="youtube_id !== ''"
+        :video-id="youtube_id"
+        player-vars={}
+        @ready="this.readyPlaying"
+        @ended="this.finishedPlaying">
+    </youtube>
 </template>
 
 <script>
+    import {mapActions, mapGetters} from "vuex";
+
     export default {
-        name: "Player"
+        name: "Player",
+        computed: {
+            ...mapGetters('playlist', ['getItems', 'getNextInQueue']),
+            // 'nowPlaying': this.getNextInQueue()
+            // player() {
+            //     return this.$refs.youtube.player;
+            // }
+        },
+        data() {
+            return {
+                'autoplay': false,
+                'youtube_id': 'u0oQymk7ZXE'
+            }
+        },
+        methods: {
+            ...mapActions('playlist', ['removeFirstFromPlaylist']),
+            created () {
+             // no sure yet
+                this.subscribe();
+            },
+            finishedPlaying() {
+                console.log('We finished playing');
+            },
+            readyPlaying() {
+                // if(this.autoplay) {
+                //     this.player.playVideo();
+                // }
+            },
+            subscribe() {
+
+            }
+        }
     }
 </script>
 
