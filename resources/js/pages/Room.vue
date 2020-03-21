14079af7e82133ab12787ebe86667c971750aea6
Sander van Kasteel <info@sandervankasteel.nl>
2020-03-21 22:55:56 +0100

    Temporarly disabled the hardcoded YoutubePlayer

--- resources/js/pages/Room.vue
+++ resources/js/pages/Room.vue
@@ -9,14 +9,15 @@
         </div>
         <div class="flex mb-4 bg-blue-600 h-full">
             <div class="w-3/4">
-                <iframe width="100%"
-                        height="100%"
-                        src="https://www.youtube.com/embed/dQw4w9WgXcQ?wmode=transparent"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen
-                        class="z-0">
-                </iframe>
+<!--                <iframe width="100%"-->
+<!--                        height="100%"-->
+<!--                        src="https://www.youtube.com/embed/dQw4w9WgXcQ?wmode=transparent"-->
+<!--                        frameborder="0"-->
+<!--                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"-->
+<!--                        allowfullscreen-->
+<!--                        class="z-0">-->
+<!--                </iframe>-->
+                <Player></Player>
             </div>
             <div class="w-1/4">
                 <span class="text-lg font-extrabold my-4 ml-2">Playing next</span>
@@ -30,10 +31,11 @@
     import Playlist from "../components/Playlist";
     import SearchBar from "../components/SearchBar";
     import {mapActions, mapState} from "vuex";
+    import Player from "../components/Player";
 
     export default {
         name: "Room",
-        components: {SearchBar, Playlist},
+        components: {Player, SearchBar, Playlist},
         computed: mapState({
             roomName: state => state.room.id
         }),
