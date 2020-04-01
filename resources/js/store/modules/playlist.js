246a49eabee5d7acbf655272e9a86bb86b7339e0
Sander van Kasteel <s.vankasteel@parkos.com>
2020-04-02 00:18:36 +0200

    Removed default items in the playlist and added the option to populate the playlist, from data known in the backend

--- resources/js/store/modules/playlist.js
+++ resources/js/store/modules/playlist.js
@@ -1,17 +1,5 @@
 const state = {
-    items: [{
-        youtube_id: 'dQw4w9WgXcQ',
-        image_url: 'https://i.ytimg.com/vi/dQw4w9WgXcQ/sddefault.jpg',
-        title: 'Rick Astley - Never Gonna Give You Up',
-        upvotes: 10,
-        downvotes: 1
-    }, {
-        youtube_id: 'QwdbFNGCkLw',
-        'image_url': 'https://i.ytimg.com/vi/QwdbFNGCkLw/default.jpg',
-        title: 'Nightcore - Ievan Polkka (VSNS Remix)',
-        upvotes: 420,
-        downvotes: 69
-    }],
+    items: [],
     loading: true
 };
 
@@ -27,17 +15,30 @@ const getters = {
 
 // actions
 const actions = {
-    addToPlaylist({ state, commit, rootState }, item) {
+    addToPlaylist({commit, rootState }, item) {
         const roomName = rootState.room.id;
+
         commit('pushToPlaylist', { item, roomName });
     },
     removeFirstFromPlaylist({ state, commit}) {
         commit('popFromPlaylist')
+    },
+    populatePlaylist({ commit, rootState}) {
+        const roomName = rootState.room.id;
+
+        window.axios.get(`/api/v1/rooms/${roomName}/playlist_items`)
+            .then((resp) => {
+                commit('populatePlaylist', { items: resp.data.data });
+            });
     }
 };
 
 const mutations = {
-    pushToPlaylist( state, { item, roomName }) {
+    populatePlaylist(state, { items }) {
+        state.items = items;
+        state.loading = false;
+    },
+    pushToPlaylist(state, { item, roomName }) {
         if(item.pushToBackend) {
             window.axios.post(`/api/v1/rooms/${roomName}/add_to_playlist`, item);
         }
@@ -51,9 +52,6 @@ const mutations = {
     removeFromPlaylist( state ) {
         state.items = state.items.slice(1);
     },
-    populatePlaylist(state) {
-        window.axios.get('/api/v1/rooms/${roomName}/playlist_items')
-    }
 };
 
 export default {
